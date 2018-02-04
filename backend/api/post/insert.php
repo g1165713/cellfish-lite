56974f3c63302e229e5dbd19670fc3077cedb513
g1165713 <36026452+g1165713@users.noreply.github.com>
2018-02-04 21:55:59 +0800

    Update insert.php

--- backend/api/post/insert.php
+++ backend/api/post/insert.php
@@ -1,18 +1,14 @@
-<?php
-require_once("../../prelude.php");
-require_once("../../src/sqlite3.php");
-require_once("../../src/image.php");
-require_once("../../src/util.php");
-
-request_method("POST");
-request_params(["title", "content"]);
-\util\admin_only();
-
-$params["id"] = \util\gen_uuidv4();
-$params["created_at"] = time();
-$params["updated_at"] = time();
-$params["image"] = \image\try_put($params["image"]);
-
-$sql = "INSERT INTO posts VALUES ( :id, :title, :content, :image, :created_at, :updated_at )";
-\sqlite3\execute($sql, $params);
-ok();
+ require_once("../../src/util.php");
+  
+  request_method("POST");
+  request_params(["title", "content", "tags"]);
+  \util\admin_only();
+  
+  $params["id"] = \util\gen_uuidv4();
+  $params["created_at"] = time();
+  $params["updated_at"] = time();
+  $params["image"] = \image\try_put($params["image"]);
+  
+  $sql = "INSERT INTO posts VALUES ( :id, :title, :content, :image, :created_at, :updated_at, :tags )";
+  \sqlite3\execute($sql, $params);
+  ok();
