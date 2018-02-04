2a63e4a3f00be30b3932c387b407fe7b7b4282d8
g1165713 <36026452+g1165713@users.noreply.github.com>
2018-02-04 13:58:31 +0800

    change_email.php
    change email.

---
+++ backend/api/auth/change_email.php
@@ -0,0 +1,10 @@
+<?php
+require_once("../../prelude.php");
+require_once("../../src/sqlite3.php");
+
+request_method("POST");
+request_params(["email","username"]);
+
+
+\sqlite3\execute("UPDATE user SET email = :email WHERE username = :username");
+ok();
