c78cda460f172a9f1fb4c414dc7999ca2282fe30
g1165713 <36026452+g1165713@users.noreply.github.com>
2018-02-04 21:58:21 +0800

    Update update.php

--- backend/api/post/update.php
+++ backend/api/post/update.php
@@ -5,14 +5,14 @@ require_once("../../src/image.php");
 require_once("../../src/util.php");
 
 request_method("POST");
-request_params(["id", "title", "content"]);
+request_params(["id", "title", "content", "tags"]);
 \util\admin_only();
 
 $params["updated_at"] = time();
 $params["image"] = \image\try_put($params["image"]);
 
 $sql = "UPDATE posts
-        SET title = :title, content = :content, image = :image, updated_at = :updated_at
+        SET title = :title, content = :content, image = :image, updated_at = :updated_at, tags = :tags
         WHERE id = :id";
 \sqlite3\execute($sql, $params);
 ok();
