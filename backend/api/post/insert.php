6d46406c1d2aa341a2d28cf21478b44c93e0e132
g1165713 <36026452+g1165713@users.noreply.github.com>
2018-02-04 13:08:05 +0800

    insert.php
    adding tags

--- backend/api/post/insert.php
+++ backend/api/post/insert.php
@@ -5,7 +5,7 @@ require_once("../../src/image.php");
 require_once("../../src/util.php");
 
 request_method("POST");
-request_params(["title", "content"]);
+request_params(["title", "content", "tags"]);
 \util\admin_only();
 
 $params["id"] = \util\gen_uuidv4();
@@ -13,6 +13,6 @@ $params["created_at"] = time();
 $params["updated_at"] = time();
 $params["image"] = \image\try_put($params["image"]);
 
-$sql = "INSERT INTO posts VALUES ( :id, :title, :content, :image, :created_at, :updated_at )";
+$sql = "INSERT INTO posts VALUES ( :id, :title, :content, :image, :created_at, :updated_at, :tags )";
 \sqlite3\execute($sql, $params);
 ok();
