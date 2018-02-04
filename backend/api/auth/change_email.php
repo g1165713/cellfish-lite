73f2f59f6ba836870eb0c3036e853728dfe41e8b
g1165713 <36026452+g1165713@users.noreply.github.com>
2018-02-04 14:06:28 +0800

    Update change_email.php

--- backend/api/auth/change_email.php
+++ backend/api/auth/change_email.php
@@ -4,7 +4,7 @@ require_once("../../src/sqlite3.php");
 
 request_method("POST");
 request_params(["email","username"]);
-
+$params["username"] = \util\login_as();
 
 \sqlite3\execute("UPDATE user SET email = :email WHERE username = :username",$params);
 ok();
